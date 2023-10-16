9b8b7f64e2e5a25e03bd784cf0c7655cd97795a9
Drewster64 <143622588+Drewster64@users.noreply.github.com>
2023-10-16 10:27:26 -0400

    Weather Project
    Weather Project

---
+++ scriptW.js
@@ -0,0 +1,26 @@
+const API_KEY = "d86ff32fe6999838a52e0a62ca3f3635"; 
+$(document).ready(function () {
+$("#checkWeather").click(function () {
+let city = $("#location").val();
+$.ajax({
+url:
+`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${API_KEY}`,type: "GET",dataType: "json",success: function (data) {
+let forecastHtml = "<h5>Pronóstico de 7 días:</h5>";
+data.list.forEach((day) => {forecastHtml += `<strong>Fecha:</strong> ${new Date(day.dt * 1000).toDateString()} ${new Date(day.dt * 1000).toTimeString()}
+<br>
+<strong>Temperatura:</strong>
+${day.main.temp}°F <br>
+<strong>Descripción:</strong> ${day.weather[0].description} 
+<br>
+<hr>`;
+});
+$("#weatherResult").html(forecastHtml);
+},
+error: function () {
+$("#weatherResult").html(
+"<p>Error al obtener el clima. Por favor, inténtalo de nuevo.</p>"
+);
+},
+});
+});
+});
