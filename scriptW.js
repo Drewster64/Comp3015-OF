ac573f0c2b03e80b5d31e206daad7b96c86c145f
Drewster64 <143622588+Drewster64@users.noreply.github.com>
2023-10-23 16:45:59 -0400

    Update scriptW.js
    mod3 act1

--- scriptW.js
+++ scriptW.js
@@ -4,15 +4,18 @@ $("#checkWeather").click(function () {
 let city = $("#location").val();
 $.ajax({
 url:
-`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${API_KEY}`,type: "GET",dataType: "json",success: function (data) {
+`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${API_KEY}`,type: "GET",dataType: "json",
+success: function (data) {
+    console.log(data)
 let forecastHtml = "<h5>Pronóstico de 7 días:</h5>";
 data.list.forEach((day) => {forecastHtml += `<strong>Fecha:</strong> ${new Date(day.dt * 1000).toDateString()} ${new Date(day.dt * 1000).toTimeString()}
 <br>
-<strong>Temperatura:</strong>
-${day.main.temp}°F <br>
+<strong>Temperatura:</strong>${day.main.temp}°F <br> 
+<img src="https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png">
 <strong>Descripción:</strong> ${day.weather[0].description} 
 <br>
-<hr>`;
+<hr>
+`;
 });
 $("#weatherResult").html(forecastHtml);
 },
