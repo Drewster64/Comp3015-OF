54e2f5339461be2000e52a96d2c3d3512c2ecca0
Drewster64 <143622588+Drewster64@users.noreply.github.com>
2023-11-12 15:09:31 -0400

    form project

---
+++ form.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var loginButton = document.getElementById('upload');
+
+    loginButton.addEventListener('click', function () {
+        alert('¡Felicidades! su registro ha sido completado. Por favor vuelva al inicio para iniciar sesión.');
+    });
+});
