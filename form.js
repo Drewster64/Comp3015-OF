7daa9d91a9f3235fbdbd460bd946875441fd5a66
Drewster64 <143622588+Drewster64@users.noreply.github.com>
2023-12-10 19:45:59 -0400

    login/register forms
    login/register forms

--- form.js
+++ form.js
@@ -1,7 +1,25 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var loginButton = document.getElementById('upload');
+//document.addEventListener('DOMContentLoaded', function () {
+    //var loginButton = document.getElementById('upload');
 
-    loginButton.addEventListener('click', function () {
-        alert('¡Felicidades! su registro ha sido completado. Por favor vuelva al inicio para iniciar sesión.');
+    //loginButton.addEventListener('click', function () {
+        //alert('¡Felicidades! su registro ha sido completado. Por favor vuelva al inicio para iniciar sesión.');
+    //});
+//});
+$(document).ready(function(){
+    $('form').submit(function(e){
+        let pass1 = $('#pass1').val();
+        let cpass = $('cpass').val();
+        if (pass1 !== cpass){
+            alert('Password and Confirmation Password must be the same')
+            e.preventDefault();
+        }
     });
+    function mostrarContrasena() {
+        var x = document.getElementById("password");
+        if (x.type === "password") {
+            x.type = "text";
+        } else {
+            x.type = "password";
+        }
+    }
 });
